cf48658965225fd82e37bb02e0149d24a892e546
Yas Darji <yashdarjicse@gmail.com>
2026-09-19 11:04:56 +0530

    fix bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${plexMono.variable} ${plexSans.variable} ${newsreader.variable}`}>
         {children}
       </body>
